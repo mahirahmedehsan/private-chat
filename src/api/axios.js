b1ec98b27985fcee9bf865069c4ad8259e788a46
Shimul <mahirahmedehsan.com>
2026-06-28 03:21:15 +0600

    14 NUM client

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,6 @@ const API_URL = import.meta.env.VITE_API_URL || ''
 
 const api = axios.create({
   baseURL: `${API_URL}/api`,
-  headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 })
 
